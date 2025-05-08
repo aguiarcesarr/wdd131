533053d8de8d07c05817f861e2fa644bec945a77
aguiarcesarr <cesarbras2020@gmail.com>
2025-05-07 21:58:06 -0300

    second part of work

---
+++ scripts/getdates.js
@@ -0,0 +1,3 @@
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+
+document.querySelector("#LastModified").textContent = `Last Modification: ${document.lastModified}`;
