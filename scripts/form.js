ca50dafdb8dc480cfe5b91231d1b0c1c68c7feb8
aguiarcesarr <cesarbras2020@gmail.com>
2025-06-11 21:58:03 -0300

    fixing js

--- scripts/form.js
+++ scripts/form.js
@@ -1,16 +1,18 @@
-const products = [
-  { id: "fc-1888", name: "Flux Capacitor" },
-  { id: "fc-2050", name: "Power Laces" },
-  { id: "fs-1987", name: "Hoverboard" },
-  { id: "ac-2000", name: "Arc Reactor" },
-  { id: "rr-2020", name: "Rocket Boots" }
-];
+document.addEventListener("DOMContentLoaded", () => {
+  const products = [
+    { id: "fc-1888", name: "Flux Capacitor" },
+    { id: "fc-2050", name: "Power Laces" },
+    { id: "fs-1987", name: "Hoverboard" },
+    { id: "ac-2000", name: "Arc Reactor" },
+    { id: "rr-2020", name: "Rocket Boots" }
+  ];
 
-const selectElement = document.querySelector("#productName");
+  const selectElement = document.querySelector("#productName");
 
-products.forEach(product => {
-  const option = document.createElement("option");
-  option.value = product.name; // Or use product.id if required
-  option.textContent = product.name;
-  selectElement.appendChild(option);
+  products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.name;
+    option.textContent = product.name;
+    selectElement.appendChild(option);
+  });
 });
